5be3f2f50ec2e789549b280cc4ed93edf6e48e8c
Oupa Moloi <oupamoloi.dev@gmail.com>
2021-01-05 13:44:40 +0200

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD-bqJewZA3kphH8vnlQ11NuG5X1-lbaeU",
+    authDomain: "nova-imprerium-fx-assessment.firebaseapp.com",
+    projectId: "nova-imprerium-fx-assessment",
+    storageBucket: "nova-imprerium-fx-assessment.appspot.com",
+    messagingSenderId: "282814242710",
+    appId: "1:282814242710:web:e760c4365a9b6ac58bc7b7",
+    measurementId: "G-B7RTLRNFD2"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD-bqJewZA3kphH8vnlQ11NuG5X1-lbaeU",
+    authDomain: "nova-imprerium-fx-assessment.firebaseapp.com",
+    projectId: "nova-imprerium-fx-assessment",
+    storageBucket: "nova-imprerium-fx-assessment.appspot.com",
+    messagingSenderId: "282814242710",
+    appId: "1:282814242710:web:e760c4365a9b6ac58bc7b7",
+    measurementId: "G-B7RTLRNFD2"
+  }
 };
 
 /*
